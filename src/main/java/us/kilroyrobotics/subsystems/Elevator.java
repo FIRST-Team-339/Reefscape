f69b167815359b51465f517ec8c658298f0193ac
Kilroy Programming <178730847+KilroyProgramming@users.noreply.github.com>
2025-01-25 16:53:18 -0500

    feat: elevator subsystem

--- src/main/java/us/kilroyrobotics/subsystems/Elevator.java
+++ src/main/java/us/kilroyrobotics/subsystems/Elevator.java
@@ -4,14 +4,55 @@
 
 package us.kilroyrobotics.subsystems;
 
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
+import static edu.wpi.first.units.Units.Inches;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import us.kilroyrobotics.Constants.CoralMechanismConstants;
+import us.kilroyrobotics.Constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase {
     /** Creates a new Elevator. */
-    public Elevator() {}
 
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
+    private SparkMax leadMotor;
+    private SparkMax followerMotor;
+    private RelativeEncoder encoder; 
+    private SparkClosedLoopController pidController;
+
+    public Elevator() {
+        this.leadMotor = new SparkMax(ElevatorConstants.kLeftMotorId, MotorType.kBrushless);
+        this.followerMotor = new SparkMax(ElevatorConstants.kRightMotorId, MotorType.kBrushless);
+        this.encoder = this.leadMotor.getEncoder();
+        this.pidController = this.leadMotor.getClosedLoopController();
+
+        // Configure
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pidf(1.0, 0.0, 0.0, 0.0);
+
+        this.leadMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        this.followerMotor.configure(new SparkMaxConfig().follow(this.leadMotor, true), ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public void setPosition(Distance distance) {
+        this.pidController.setReference(distance.in(Inches), ControlType.kPosition);
+    }
+
+    public void resetPosition() {
+        encoder.setPosition(0);
+    }
+
+    public void stop() {
+        this.leadMotor.setVoltage(0.0);
     }
 }
