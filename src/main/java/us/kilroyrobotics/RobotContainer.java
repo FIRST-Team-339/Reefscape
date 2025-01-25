46ae546040bb8652ea6a66f2e8b952d9ade4ad1b
Kilroy Programming <178730847+KilroyProgramming@users.noreply.github.com>
2025-01-25 16:53:13 -0500

    added pre-set elevator commands

--- src/main/java/us/kilroyrobotics/RobotContainer.java
+++ src/main/java/us/kilroyrobotics/RobotContainer.java
@@ -14,11 +14,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import us.kilroyrobotics.Constants.DriveConstants;
+import us.kilroyrobotics.Constants.ElevatorConstants;
 import us.kilroyrobotics.generated.TunerConstants;
 import us.kilroyrobotics.subsystems.CommandSwerveDrivetrain;
+import us.kilroyrobotics.subsystems.Elevator;
 
 public class RobotContainer {
     private double kTeleopMaxSpeed =
@@ -42,9 +46,13 @@ public class RobotContainer {
 
     private final Telemetry logger = new Telemetry(kTeleopMaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandXboxController driverController = new CommandXboxController(0);
+    private final CommandJoystick leftOperatorJoystick = new CommandJoystick(1);
+    private final CommandJoystick rightOperatorJoystick = new CommandJoystick(2);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+    private final Elevator elevatorSubsystem = new Elevator();
 
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
@@ -70,6 +78,27 @@ public class RobotContainer {
                         }
                     });
 
+        public Command elevatorSetL1 = Commands.runOnce(
+                    () ->
+                        elevatorSubsystem.setPosition(ElevatorConstants.kL1Height)
+                    , elevatorSubsystem);
+        public Command elevatorSetL2 = Commands.runOnce(
+                    () ->
+                        elevatorSubsystem.setPosition(ElevatorConstants.kL2Height)
+                    , elevatorSubsystem);
+        public Command elevatorSetL3 = Commands.runOnce(
+                    () ->
+                        elevatorSubsystem.setPosition(ElevatorConstants.kL3Height)
+                    , elevatorSubsystem);
+        public Command elevatorSetL4 = Commands.runOnce(
+                    () ->
+                        elevatorSubsystem.setPosition(ElevatorConstants.kL4Height)
+                    , elevatorSubsystem);
+        public Command elevatorSetCoralStation = Commands.runOnce(
+                    () ->
+                        elevatorSubsystem.setPosition(ElevatorConstants.kCoralStationHeight)
+                    , elevatorSubsystem);
+
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
@@ -78,57 +107,63 @@ public class RobotContainer {
                 drivetrain.applyRequest(
                         () ->
                                 drive.withVelocityX(
-                                                -joystick.getLeftY()
+                                                -driverController.getLeftY()
                                                         * kTeleopMaxSpeed) // Drive forward with
                                         // negative Y
                                         // (forward)
                                         .withVelocityY(
-                                                -joystick.getLeftX()
+                                                -driverController.getLeftX()
                                                         * kTeleopMaxSpeed) // Drive left with
                                         // negative X
                                         // (left)
                                         .withRotationalRate(
-                                                -joystick.getRightX()
+                                                -driverController.getRightX()
                                                         * kMaxAngularRate) // Drive counterclockwise
                         // with
                         // negative X (left)
                         ));
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b()
+        driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        driverController.b()
                 .whileTrue(
                         drivetrain.applyRequest(
                                 () ->
                                         point.withModuleDirection(
                                                 new Rotation2d(
-                                                        -joystick.getLeftY(),
-                                                        -joystick.getLeftX()))));
+                                                        -driverController.getLeftY(),
+                                                        -driverController.getLeftX()))));
 
-        joystick.pov(0)
+        driverController.pov(0)
                 .whileTrue(
                         drivetrain.applyRequest(
                                 () -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
-        joystick.pov(180)
+        driverController.pov(180)
                 .whileTrue(
                         drivetrain.applyRequest(
                                 () -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start()
-                .and(joystick.y())
+        driverController.back().and(driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        driverController.back().and(driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        driverController.start()
+                .and(driverController.y())
                 .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start()
-                .and(joystick.x())
+        driverController.start()
+                .and(driverController.x())
                 .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // Reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        driverController.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         // Toggle between high and low speeds
-        joystick.x().onTrue(toggleMaxSpeed);
+        driverController.x().onTrue(toggleMaxSpeed);
+
+        rightOperatorJoystick.button(1).onTrue(elevatorSetL1);
+        rightOperatorJoystick.button(2).onTrue(elevatorSetL2);
+        rightOperatorJoystick.button(3).onTrue(elevatorSetL3);
+        rightOperatorJoystick.button(4).onTrue(elevatorSetL4);
+        rightOperatorJoystick.button(5).onTrue(elevatorSetCoralStation);
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
