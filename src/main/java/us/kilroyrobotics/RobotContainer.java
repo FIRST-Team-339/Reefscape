7e881466533913b90b994b0f64a0e7a95dcc90f2
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2025-01-27 08:57:28 -0500

    fix: teleop speed limiter (#12)
    * chore: import new tuner constants
    
    * feat: toggle between high & low speeds with `x` button
    
    * chore: vsc formatting settings
    
    * chore: update pathplanner config
    
    * fix: revert changes and use pre-existing slow speed modes via controller dpad
    
    * fix: incorrect merge
    
    ---------
    
    Signed-off-by: Fisher <72705185+fisherjacobc@users.noreply.github.com>
    Co-authored-by: Kilroy Programming <178730847+KilroyProgramming@users.noreply.github.com>
    Signed-off-by: Podcakero <github@podcake.net>

--- src/main/java/us/kilroyrobotics/RobotContainer.java
+++ src/main/java/us/kilroyrobotics/RobotContainer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import us.kilroyrobotics.Constants.DriveConstants;
@@ -29,7 +28,7 @@ import us.kilroyrobotics.subsystems.Elevator;
 import us.kilroyrobotics.subsystems.Wrist;
 
 public class RobotContainer {
-    private double kTeleopMaxSpeed =
+    private double kMaxSpeed =
             TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double kMaxAngularRate =
             RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
@@ -38,7 +37,7 @@ public class RobotContainer {
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive =
             new SwerveRequest.FieldCentric()
-                    .withDeadband(kTeleopMaxSpeed * 0.1)
+                    .withDeadband(kMaxSpeed * 0.1)
                     .withRotationalDeadband(kMaxAngularRate * 0.1) // Add a 10% deadband
                     .withDriveRequestType(
                             DriveRequestType
@@ -48,7 +47,7 @@ public class RobotContainer {
     private final SwerveRequest.RobotCentric forwardStraight =
             new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
-    private final Telemetry logger = new Telemetry(kTeleopMaxSpeed);
+    private final Telemetry logger = new Telemetry(kMaxSpeed);
 
 	/* Controllers */
 	private final CommandXboxController driverController = new CommandXboxController(0);
@@ -77,20 +76,6 @@ public class RobotContainer {
         configureBindings();
     }
 
-    /** Toggles the max speed via a one-time command */
-    public Command toggleMaxSpeed =
-            Commands.runOnce(
-                    () -> {
-                        if (this.kTeleopMaxSpeed
-                                == TunerConstants.kSpeedAt12Volts.in(MetersPerSecond)) {
-                            this.kTeleopMaxSpeed =
-                                    DriveConstants.kLowDriveSpeed.in(MetersPerSecond);
-                        } else {
-                            this.kTeleopMaxSpeed =
-                                    TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
-                        }
-                    });
-
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
@@ -100,12 +85,11 @@ public class RobotContainer {
                         () ->
                                 drive.withVelocityX(
                                                 -joystick.getLeftY()
-                                                        * kTeleopMaxSpeed) // Drive forward with
+                                                        * kMaxSpeed) // Drive forward with
                                         // negative Y
                                         // (forward)
                                         .withVelocityY(
-                                                -joystick.getLeftX()
-                                                        * kTeleopMaxSpeed) // Drive left with
+                                                -joystick.getLeftX() * kMaxSpeed) // Drive left with
                                         // negative X
                                         // (left)
                                         .withRotationalRate(
@@ -128,11 +112,34 @@ public class RobotContainer {
         joystick.pov(0)
                 .whileTrue(
                         drivetrain.applyRequest(
-                                () -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
+                                () ->
+                                        forwardStraight
+                                                .withVelocityX(DriveConstants.kLowDriveSpeed)
+                                                .withVelocityY(0)));
+        joystick.pov(90)
+                .whileTrue(
+                        drivetrain.applyRequest(
+                                () ->
+                                        forwardStraight
+                                                .withVelocityX(0)
+                                                .withVelocityY(
+                                                        DriveConstants.kLowDriveSpeed
+                                                                .unaryMinus())));
         joystick.pov(180)
                 .whileTrue(
                         drivetrain.applyRequest(
-                                () -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
+                                () ->
+                                        forwardStraight
+                                                .withVelocityX(
+                                                        DriveConstants.kLowDriveSpeed.unaryMinus())
+                                                .withVelocityY(0)));
+        joystick.pov(270)
+                .whileTrue(
+                        drivetrain.applyRequest(
+                                () ->
+                                        forwardStraight
+                                                .withVelocityX(0)
+                                                .withVelocityY(DriveConstants.kLowDriveSpeed)));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
@@ -147,9 +154,6 @@ public class RobotContainer {
 
         // Reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
-
-        // Toggle between high and low speeds
-        joystick.x().onTrue(toggleMaxSpeed);
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
